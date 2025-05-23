010d7cf9c6477b3c419448d1d90b227aeb75e42c
peterhoward42 <peterhoward42@gmail.com>
2025-05-23 16:12:22 +0100

    Iterating on image viability - wip

--- src/components/HeroSection.svelte
+++ src/components/HeroSection.svelte
@@ -1,35 +1,17 @@
-
 <script>
-  let currentImage = 0;
-  const images = [
-    '/src/assets/images/image1.jpg',
-    '/src/assets/images/image2.jpg',
-    '/src/assets/images/image3.jpg',
-    '/src/assets/images/image4.jpg',
-    '/src/assets/images/image5.jpg',
-    '/src/assets/images/image6.jpg',
-    '/src/assets/images/image7.jpg',
-    '/src/assets/images/image8.jpg'
-  ];
-
-  const cycleImages = () => {
-    setInterval(() => {
-      currentImage = (currentImage + 1) % images.length;
-    }, 3000);
-  };
-
-  cycleImages();
+  import apple from "/src/assets/images/apple.png";
 </script>
 
 <section class="hero">
   <h1>DrawExact</h1>
   <h2>Powerful precision drawing, effortlessly.</h2>
   <div class="image-container">
-    {#each images as image, index}
-      <img src={image} alt="Hero image {index + 1}" class:image-visible={index === currentImage} />
-    {/each}
+    <img src={apple} alt="alt name for apple" />
   </div>
-  <button on:click={() => window.location.href = 'https://drawexact.com'}>Take me to DrawExact</button>
+
+  <button on:click={() => (window.location.href = "https://drawexact.com")}
+    >Take me to DrawExact</button
+  >
 </section>
 
 <style>
@@ -48,11 +30,8 @@
     left: 0;
     max-width: 100%;
     height: auto;
-    opacity: 0;
-    transition: opacity 1s ease-in-out;
-  }
-  .image-visible {
     opacity: 1;
+    transition: opacity;
   }
   button {
     width: 100%;
