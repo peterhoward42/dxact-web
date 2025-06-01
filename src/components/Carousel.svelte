ea5fe29f8f7268e9ebfe8f4a00db7e4ee93baf2f
peterhoward42 <peterhoward42@gmail.com>
2025-06-01 15:28:50 +0100

    Make carousel cleaner by exploiting onMount

--- src/components/Carousel.svelte
+++ src/components/Carousel.svelte
@@ -3,53 +3,55 @@
     import apple from "/src/assets/images/apple.png";
     import chain from "/src/assets/images/chain.png";
     import weather from "/src/assets/images/weather.png";
+    import lines from "/src/assets/images/lines.png";
+    import arcs from "/src/assets/images/arcs.png";
+    import { onMount } from "svelte";
+
+    let aspectRatioKnown = false;
 
     // images are the imported "src" attributes for the images we'll show
-    const images = [apple, chain, weather];
+    const images = [apple, chain, weather, lines, arcs];
 
-    // imageRefs are javascript variables that point to the Img html elements that get instantiated.
+    // imageRefs give us javascript variables that point to the Img html elements that get instantiated.
     // See bind:this in the layout.
     let imgRefs = [];
 
-    // Incumbent and next are the indexes that define the image that is being shown,
+     // Incumbent and next are the indexes that define the image that is being shown,
     // and the one that will be shown next.
     let incumbent = 0;
     let next = mod(incumbent + 1);
 
-    function mod(n) {
-        return n % images.length;
-    }
-
-    async function handleImageLoaded(evt, index) {
-        // Once we know the (universal) aspect ratio of the images in the set, we can
-        // use it to set the aspect ratio of the container.
-        if (index == 0) {
+    onMount(async () => {
+        if (!aspectRatioKnown) {
             const aspectRatio =
-                evt.target.naturalWidth / evt.target.naturalHeight;
+                imgRefs[0].naturalWidth / imgRefs[0].naturalHeight;
             console.log("XXXX aspect ratio found: ", aspectRatio);
         }
-        // Only as each image is loaded can we initialise its state.
-        if (index == incumbent || index == next) {
-            setVisible(index)
-        } else {
-            setInVisible(index)
+        for (const [i, imgRef] of imgRefs.entries()) {
+            if (i == incumbent) {
+                setPositionCentre(i);
+                setVisible(i);
+            } else if (i==next){
+                setPositionLeft(i)
+                setVisible(i);
+            } else {
+                setInVisible(i)
+            }
         }
-        // Manually trigger the iteration frame function at t0, because setInterval()
-        // doesn't do that.
-        await makeFrame()
-    }
+    });
 
+   
+    // makeFrame is the per-frame function that produces the state changes that produce
+    // the desired animation.
     async function makeFrame() {
-        // Slide next in to replace incumbent.
+        // Slide the next image in, to replace the incumbent, while at the same time,
+        // sliding the incumbent out to the right. I.e. create a "push" effect.
         setPositionRight(incumbent);
         setPositionCentre(next);
 
         await tick();
 
-        // The one got shifted out (incumbent) drops back to being invisible.
-        setInVisible(incumbent);
-
-        // Update the indices for incumbent and next ready for next timed transition.
+        // Update the indices for incumbent and next ready for the next animation iteration.
         incumbent = mod(incumbent + 1);
         next = mod(incumbent + 1);
 
@@ -58,8 +60,14 @@
         setVisible(next);
     }
 
+    // This is the timing-based animation event generator.
     const interval = setInterval(makeFrame, 2000);
 
+    // The functions below have the effect of setting the targeted image
+    // to the position stated in the function name.
+    //
+    // It does that by giving the image a class to do that, and at the same time
+    // REMOVING the classes it may have that compete.
     function setPositionRight(index) {
         imgRefs[index].classList.remove(goLeft);
         imgRefs[index].classList.remove(goCentre);
@@ -78,6 +86,8 @@
         imgRefs[index].classList.add(goLeft);
     }
 
+    // Similarly we change the image's display attribute by giving it
+    // a corresponding class - and removing any that compete with it.
     function setVisible(index) {
         imgRefs[index].classList.remove(hide);
         imgRefs[index].classList.add(show);
@@ -94,18 +104,15 @@
 
     const hide = "hide";
     const show = "show";
+
+    function mod(n) {
+        return n % images.length;
+    }
 </script>
 
 <div class="carousel">
     {#each images as src, index}
-        <img
-            {src}
-            alt=""
-            bind:this={imgRefs[index]}
-            on:load={(evt, el) => {
-                handleImageLoaded(evt, index);
-            }}
-        />
+        <img {src} alt="" bind:this={imgRefs[index]} />
     {/each}
 </div>
 
@@ -126,7 +133,7 @@
         height: 100%;
         object-fit: cover;
         pointer-events: none;
-        /* transition: left 1s ease-in-out; */
+        transition: left 1s ease-in-out;
     }
 
     :global(.goright) {
