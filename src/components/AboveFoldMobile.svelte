c46d34e7fb1ae86467cb34990ceda33808cf579b
peterhoward42 <peterhoward42@gmail.com>
2025-11-15 21:51:10 +0000

    Hero video design strategy - first step

--- src/components/AboveFoldMobile.svelte
+++ src/components/AboveFoldMobile.svelte
@@ -1,38 +1,48 @@
 <script>
-    import ComposedImages from "./carousel/ComposedImages.svelte";
-    import { introCarouselFrames } from "./carousel/intro/frames.svelte.js";
-
-    import CTA from "./CTA.svelte";
 </script>
 
 <div class="abovefoldmobile qcol">
-    <span class="qe1 head">DrawExact</span>
-
-    <span class="subhead qe1"
-        >Is an app for making drawings in your browser</span
-    >
-
-    <ComposedImages carouselFrames={introCarouselFrames}></ComposedImages>
-    <div class="cta"><CTA /></div>
+    <span class="qhead qe1 head">DrawExact</span>
+    <span class="">A new kind of drawing environment</span>
+    <span class="fluent">Exact drawing, made fluent</span>
+    <em class="story">This is a story about three superpowers</em>
+    <em class="unfold">See them unfold ...</em>
+    <div class="video-wrapper">
+        <iframe
+            width="100%"
+            height="100%"
+            src="https://www.youtube.com/embed/gtXHXOQT208?si=yEutBLrth50MixnM"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+        ></iframe>
+    </div>
 </div>
 
 <style>
     .abovefoldmobile {
         width: 100%;
+        padding-top: 1rem;
         align-items: center;
     }
-
     .head {
-        font-size: 2rem;
-        padding-top: 1rem;
-        padding-bottom: 1rem;
+        margin-bottom: 0.75rem;
     }
-    .subhead {
-        padding-bottom: 2rem;
+    .fluent{
+        margin-top: 0.45rem;
+    }
+    .story{
+        margin-top:1rem;
+    }
+    .unfold{
+        margin-top:0.5rem;
     }
 
-    .cta {
-        align-self: center;
-        margin-top: 2rem;
+    .video-wrapper {
+        width: 100%;
+        margin-top: 1rem;
+        aspect-ratio: 16 / 9;
     }
 </style>
